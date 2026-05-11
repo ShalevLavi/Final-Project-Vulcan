6ddd44206cb42042300f85e71b7eb17378d897e5
Shalev <shalevlavi44@gmail.com>
2026-05-11 17:54:15 +0300

    fix: Fixed code in Zustand store

---
+++ CLient/src/store/useModalStore.ts
@@ -0,0 +1,13 @@
+import { create } from 'zustand'
+
+interface ModalStore {
+    isLoginOpen: boolean
+    openLogin: () => void
+    closeLogin: () => void
+}
+
+export const useModalStore = create<ModalStore>((set) => ({
+    isLoginOpen: false,
+    openLogin: () => set({ isLoginOpen: true }),
+    closeLogin: () => set({ isLoginOpen: false }),
+}))
